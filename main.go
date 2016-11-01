fdf65575b83c5240e11be35383f8747054f4d449
panyingyun <panyingyun@gmail.com>
2016-11-01 19:51:44 +0800

    add every day

--- main.go
+++ main.go
@@ -14,13 +14,35 @@ func (s *stat) Run() {
 	log.Println(s.mac)
 }
 
+type day struct {
+	id int
+}
+
+func (d *day) Run() {
+	log.Println(d.id)
+}
+
+//every 5 second run a job
+var spec5s = "0-59/5 * * * * *"
+
+//every day(00:00:00) run a job
+var specday = "0 0 0 1-31 * *"
+
 func main() {
-	spec := "0-59/5 * * * * *"
+	c := cron.New()
+
 	st := &stat{
 		mac: 1000,
 	}
-	c := cron.New()
-	c.AddJob(spec, st)
+
+	c.AddJob(spec5s, st)
+
+	day := &day{
+		id: 9000,
+	}
+
+	c.AddJob(specday, day)
+
 	c.Start()
 
 	select {}
